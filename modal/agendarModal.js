7ce6b647be1d24d7f97f18fec3a723b88e51a400
Eduarda Rodrigues Tavares <ETavares@brasal.com.br>
2023-11-20 19:15:51 -0300

    Arrumando agenda.

--- modal/agendarModal.js
+++ modal/agendarModal.js
@@ -7,15 +7,21 @@ async function postAgendarModal(req) {
     const numSessao = req.body.sessao
     const relacionamentoID = req.body.dados[0].relacionamentoID
 
+    const agendaExistente = 'SELECT * FROM sessoesmentoria WHERE RelacionamentoID = ? AND NumeroSessao = ?'
     const query = 'INSERT INTO sessoesmentoria (Data, Hora, Local, NumeroSessao, RelacionamentoID) VALUES (?, ?, ?, ?, ?)'
 
     try {
-        // Usando uma Promise para lidar com o resultado
-        const rows = await db.exec(query, [data, hora, local, numSessao, relacionamentoID])
-
-        return rows;
+        const rowsAgenda = await db.exec(agendaExistente, [relacionamentoID, numSessao]);
+        if (rowsAgenda.length === 0) {
+            // No existing session, proceed with insertion
+            const rows = await db.exec(query, [data, hora, local, numSessao, relacionamentoID]);
+            return rows;
+        } else {
+            // Session already exists
+            return "Sessão já existe.";
+        }
     } catch (error) {
-        console.error('Erro ao executar a consulta:', error)
+        console.error('Erro ao executar a consulta:', error);
         throw error;
     }
 }
